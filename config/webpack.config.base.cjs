fa4d9d619aaafc2c559278b7937e83e3c52df7d7
KurosakiRei <KurosakiRei@users.noreply.github.com>
2023-02-19 18:26:58 -0500

    Add vue3 template, Modify webpack config to support vue3, and degrade @types/greasemonkey to 3.0 that makes the script can support  more GM_* function

--- config/webpack.config.base.cjs
+++ config/webpack.config.base.cjs
@@ -1,6 +1,7 @@
 const path = require("path");
 
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const { VueLoaderPlugin } = require('vue-loader')
 
 const webpackConfig = {
   resolve: {
@@ -24,6 +25,13 @@ const webpackConfig = {
         test: /\.m?ts$/,
         use: {
           loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+            appendTsSuffixTo: [
+              '\\.vue$'
+            ],
+            happyPackMode: false
+          }
         },
       },
       {
@@ -33,10 +41,72 @@ const webpackConfig = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
+      },{
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },{
+        test: /\.vue$/,
+        use: [
+          /* config.module.rule('vue').use('vue-loader') */
+          {
+            loader: 'vue-loader',
+          }
+        ]
+      },/* config.module.rule('svg') */
+      {
+        test: /\.(svg)(\?.*)?$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name].[hash:8][ext]'
+        }
+      },
+      /* config.module.rule('images') */
+      {
+        test: /\.(png|jpe?g|gif|webp|avif)(\?.*)?$/,
+        type: 'asset',
+        generator: {
+          filename: 'img/[name].[hash:8][ext]'
+        }
+      },
+      /* config.module.rule('media') */
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        type: 'asset',
+        generator: {
+          filename: 'media/[name].[hash:8][ext]'
+        }
+      },
+      /* config.module.rule('fonts') */
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+        type: 'asset',
+        generator: {
+          filename: 'fonts/[name].[hash:8][ext]'
+        }
       },
     ],
   },
-  plugins: process.env.npm_config_report ? [new BundleAnalyzerPlugin()] : [],
+  plugins: process.env.npm_config_report ? [new BundleAnalyzerPlugin()] : [new VueLoaderPlugin()],
 };
 
 module.exports = webpackConfig;
